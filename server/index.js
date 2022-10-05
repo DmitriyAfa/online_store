d7f92d04a83e600e7eaefcc004f58454099c7f9f
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-10-05 20:02:40 +0500

    Description of the listen method

--- server/index.js
+++ server/index.js
@@ -4,4 +4,6 @@ const PORT = 5000;
 
 const app = express(); // создадим объект вызвав f-ю express. Запуск приложения
 
+// app.listen([port[, host[, backlog]]][, callback])
+// Binds and listens for connections on the specified host and port. This method is identical to Node’s http.Server.listen().
 app.listen(PORT, () => console.log("Server started on port ", PORT));
