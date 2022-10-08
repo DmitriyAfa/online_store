f92243f8683071c7bef3c03f8b2e1cedded5fee7
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-10-08 06:50:46 +0500

    userController ---> registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,30 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
+const generateJWT = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 class UserController {
-  async registrtion(req, res) {}
+  async registrtion(req, res) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      // This is a simple version of validation.
+      return next(ApiError.badRequiest("incorrect email or password"));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequiest("The user with the name exists"));
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJWT(user.id, user.email, user.role);
+    return res.json({ token });
+  }
   async login(req, res) {}
   async check(req, res, next) {
     const { id } = req.query;
