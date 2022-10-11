fae04f6e984af2594efdee3617b032d563b34adb
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-10-11 12:22:12 +0500

    AuthPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { Context } from "./index";
 import { AppRouter } from "./components/AppRouter";
 import { NavBar } from "./components/NavBar";
-const App = () => {
+import { check } from "./http/userApi";
+import { Spinner } from "react-bootstrap";
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    check()
+      .then(() => {
+        user.setUser(true);
+        user.setIsAuth(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="grow" />;
+  }
+
   return (
     <BrowserRouter>
       <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
