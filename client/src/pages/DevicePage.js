895b437d75a8937aaf857f3a39cfbdf34edd6519
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-10-12 14:38:15 +0500

    Get a device for DevicePage

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,19 +1,25 @@
+import { useEffect, useState } from "react";
 import { Col, Card, Image, Container, Row, Button } from "react-bootstrap";
 import bigStar from "../assets/bigStar.png";
+import { useParams } from "react-router-dom";
+import { fetchOneDevice } from "../http/deviceApi";
+
 export const DevicePage = () => {
-  const device = { id: 1, name: "12 pro", price: 100000, rating: 0, img: "" };
-  const description = [
-    { id: 1, title: "Оперативная память", description: "5 гб" },
-    { id: 2, title: "Камера", description: "12 мп" },
-    { id: 3, title: "Процессор", description: "Пентиум 3" },
-    { id: 4, title: "Ядра", description: "2" },
-    { id: 5, title: "Аккумулятор", description: "4000" },
-  ];
+  const [device, setDevice] = useState({ info: [] });
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchOneDevice(id).then((data) => setDevice(data));
+  });
   return (
     <Container className="mt-3">
       <Row>
         <Col md={4}>
-          <Image width={300} height={300} src={device.img} />
+          <Image
+            width={300}
+            height={300}
+            src={process.env.REACT_APP_API_URL + device.img}
+          />
         </Col>
         <Col md={4}>
           <Row className="d-flex flex-column align-items-center">
@@ -49,7 +55,7 @@ export const DevicePage = () => {
       </Row>
       <Row className="d-flex flex-column m-3">
         <h1>Характеристики</h1>
-        {description.map((info, index) => {
+        {device.info.map((info, index) => {
           return (
             <Row
               key={info.id}
